b4dd83fb818b71ac6adee7458033def9547524d1
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-08-25 13:37:26 -0700

    finished business logic and starting on UI logic
    Co-authored-by: Jaki Widby <jaki.widby@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,46 @@
+// select movie; time of day; their age
+// display price of movie ticket based on their age, time of day and movie such as first release, matinee and non-first release and senior
+
+// Business Logic
+
+function Ticket(age, time, movieType) {
+  this.age = age,
+  this.time = time,
+  this.movieType = movieType
+}
+// Regular ticket price is $14
+
+let ticketValues = Object.values(Ticket);
+console.log(ticketValues);
+
+
+Ticket.prototype.ticketPrice = function() {
+  let regularPriceTicket = 14;
+  
+  ticketValues.forEach(function() {
+    if (ticketValues[0] >= 60 && ticketValues[1] === "matinee" && ticketValues[2] === "non-regular" ) {
+      regularPriceTicket -= 6;
+      return regularPriceTicket;
+    } else if (ticketValues[0] >= 60 && ticketValues[1] === "matinee" || ticketValues[0] >= 60 && ticketValues[2] === "non-regular" || ticketValues[1] === "matinee" && ticketValues[2] === "non-regular") {
+      regularPriceTicket -= 4;
+      return regularPriceTicket;
+    } else if (ticketValues[0] >= 60 || ticketValues[1] === "matinee" || ticketValues[2] === "non-regular" ) {
+    regularPriceTicket -= 2;
+    return regularPriceTicket;
+    } else {
+      return regularPriceTicket;
+    }
+  });
+}
+
+
 $(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    const ageInput = parseInt($("#age").val());
+    const timeInput = $("#time").val();
+    const movieTypeInput = $("#movieType").val();
+
+  });
   
-  
-  event.preventDefault();
 });
